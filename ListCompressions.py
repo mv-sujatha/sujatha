25f1e4d5ddc8d3a12908512c79c52fa2a4e7201e
SUJATHA <132375629+mv-sujatha@users.noreply.github.com>
2023-05-19 10:32:26 +0530

    Update and rename ListCompressions.txt to ListCompressions.py

---
+++ ListCompressions.py
@@ -0,0 +1,14 @@
+def list_comprenssion(x,y,z,n,arr):
+    arr=[]
+    for i in range(0,x+1):
+        for j in range(0,y+1):
+            for k in range(0,z+1):
+                if sum([i+j+k!=n]):
+                    arr.append([i,j,k])
+    print(arr)
+a=int(input("enter any number"))
+b=int(input("enter any number"))
+c=int(input("enter any number"))
+d=int(input("enter any number"))
+list1=[]
+list_comprenssion(a,b,c,d,list1)
